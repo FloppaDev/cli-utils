0e80e860d0a4f5d32522c36b9ba01888b7e53fb2
FloppaDev <rschuhler@hotmail.fr>
2024-02-09 17:56:29 +0100

    commands descriptions

--- include.sh
+++ include.sh
@@ -1,32 +1,32 @@
 
-# bgl
-#
-# -f:   Only list files open in vim
-# -a:   Only list arguments
+# bgl       List background jobs
+# -f:       Only list files open in vim
+# -a:       Only list arguments
 bgl() {
     jobs | php $CLI_UTILS/lib/bgl.php $@
 }
 
-# up
-# -     A single hexadecimal character
+# up        List backgroung jobs and interactively open one
+# -         A single hexadecimal character
 up() {
     jobs | php $CLI_UTILS/lib/bgl.php
     eval "$(php $CLI_UTILS/lib/up.php $@)"
 }
 
-# paths
-#
-# $1:   Strict name or pattern of a path
+# at        List common paths from ./data/paths.json
+# $1:       Strict name or pattern of a path
 at() {
     php $CLI_UTILS/lib/paths.php $@ 
 }
 
+# to        Go to a path from ./data/paths.json
+# $1:       Strict name or pattern of a path
 to() {
     [ -z "$@" ] && at || cd $(at $@)
 }
 
 # copy
-# -|$1 Copy to clipboard stdin or $1
+# -|$1      Copy to clipboard stdin or $1
 # 
 # echo ':)' | copy
 # copy file.txt
@@ -34,23 +34,30 @@ copy() {
     [ ! -t 0 ] && (cat | xclip -sel clip) || xclip -sel clip $@
 }
 
-# paste
-# Output the contents of the clipboard
+# paste     Output the contents of the clipboard
 paste() {
     xclip -o -selection clipboard
 }
 
-# pick
-# Outputs the first file path in the clipboard.
+# pick      Output paths (1 by default) from the clipboard.
+# -e:       Only paths that contains a file extension
+# -a        All paths
+# -f        Only files
+# -d        Only directories
 #
-# -e:   
-# -a   
-# -f   
-# -F   
+# pick -af
 pick() {
     [ ! -t 0 ] && cat || paste | php $CLI_UTILS/lib/pick.php $@ 
 }
 
+# vi        Wrapper around vim
+# $1        Path to open in vim, can include line and column numbers
+#
+# vi file.txt:10:50
+# vi file.txt:10
+# vi file.txt
+# vi
+# echo 'Hey' | vi
 vi() {
     if [ ! -t 0 ]; then
         cat | vim -
@@ -61,8 +68,7 @@ vi() {
     fi
 }
 
-# serve
-#
+# serve     Start a server in current directory
 # $1:   Port
 # $2:   Router
 serve() {
@@ -70,10 +76,13 @@ serve() {
     php -S 127.0.0.1:$port $@
 }
 
+# ps1       Sets the PS1 variable
 ps1() {
     export PS1="$(php $CLI_UTILS/lib/ps1.php)"
 }
 
+# lf        Simple wrapper to format the output of ugrep nicely
+# $1        Search pattern
 lf() {
     tput rmam
     ugrep --json --line-number --column-number $@ | php $CLI_UTILS/lib/lf.php
